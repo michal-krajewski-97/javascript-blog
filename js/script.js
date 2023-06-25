9902c8fad0d77da520ff020774beff67501947a3
michal.krajewski <michal.krajewski.7991@gmail.com>
2023-06-25 03:04:10 +0200

    generating links

--- js/script.js
+++ js/script.js
@@ -30,8 +30,36 @@ const titleClickHandler = function(event){
   targetArticle.classList.add('active');
 }
 
-const links = document.querySelectorAll('.titles a');
+const optArticleSelector = '.post',
+optTitleSelector = '.post-title',
+optTitleListSelector = '.titles';
 
-for(let link of links){
-  link.addEventListener('click', titleClickHandler);
+function generateTitleLinks(){
+
+  /* remove contents of titleList */
+  const titleList = document.querySelector(optTitleListSelector);
+  
+  document.querySelector('.list.titles').innerHTML = '';
+  /* for each article */
+  const articles = document.querySelectorAll(optArticleSelector);
+
+  for(let article of articles){
+
+  /* get the article id */
+    const articleId = article.getAttribute('id');
+  /* find the title element */
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+  /* get the title from the title element */
+
+  /* create HTML of the link */
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>'; 
+  /* insert link into titleList */
+    titleList.innerHTML = titleList.innerHTML + linkHTML;
+  }
+  const links = document.querySelectorAll('.titles a');
+  for (let link of links){
+    link.addEventListener('click', titleClickHandler);
+  }
 }
+
+generateTitleLinks();
